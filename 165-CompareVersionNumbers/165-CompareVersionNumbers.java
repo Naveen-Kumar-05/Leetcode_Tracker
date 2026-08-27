1079291656e6def5a7e0c29757f85f68c71a828e
Naveenkumar S <naveenabi005@gmail.com>
2026-08-27 09:58:07 +0530

    Create 165-CompareVersionNumbers

---
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.java
@@ -0,0 +1,26 @@
+// Last updated: 27/8/2026, 9:58:02 am
+class Solution {
+    public int compareVersion(String version1, String version2) {
+
+        String[] a = version1.split("\\.");
+        String[] b = version2.split("\\.");
+
+        int n = Math.max(a.length, b.length);
+
+        for (int i = 0; i < n; i++) {
+
+            int num1 = (i < a.length) ? Integer.parseInt(a[i]) : 0;
+            int num2 = (i < b.length) ? Integer.parseInt(b[i]) : 0;
+
+            if (num1 < num2) {
+                return -1;
+            }
+
+            if (num1 > num2) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
